081a5e7135b74623bc2a8a66a7c8249ca6d6bb0f
Jainam <85880788+JAINAM576@users.noreply.github.com>
2024-07-10 18:24:10 +0530

    Time: 1 ms (99.42%), Space: 42.1 MB (26.85%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,25 +1,22 @@
 class Solution {
-      public static int minOperations(String[] logs) {
-        String path="Main";
+   public static int minOperations(String[] logs) {
+        int ans=0;
         for(String s:logs){
             int root=s.indexOf("/");
             int dot=s.indexOf('.');
             int backdot=s.lastIndexOf('.');
           if(root!=-1 && dot==-1)
           {
-     
-            path+="/"+s.substring(0,root);
+            ans++;
             
           }
           else if(root!=-1 && (dot!=backdot)){
-          
-int indexOf=path.lastIndexOf("/");
-if(indexOf!=-1)
-path=path.substring(0, indexOf);
+      if(ans>0)    
+            ans--;
 
           }
         }
-       return (path.split("/").length-1);
+       return ans;
     
     }
 }
