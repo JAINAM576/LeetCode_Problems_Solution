2e5fdb0a5778a6f38b48765bb3c2f41273b94f71
Jainam <85880788+JAINAM576@users.noreply.github.com>
2024-07-23 11:31:13 +0530

    Time: 0 ms (100.00%), Space: 14.1 MB (93.80%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        vector<int> ans;
+        vector<int> counting(201,0);
+        map<int,int> mp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            counting[nums[i]+100]++;
+            
+        }
+        for(int i=0;i<201;i++){
+            if(counting[i]==0)continue;
+            mp[counting[i]]++;
+        }
+        for(auto it:mp){
+            cout<<it.first<<"->"<<it.second<<endl;
+            if(it.second>1){
+for(int i=200;i>=0;i--){
+    if(counting[i]==it.first){
+    int val=it.first;
+        while(val--){
+        ans.push_back(i-100);
+        }
+    }
+}
+            }
+            else{
+                for(int i=0;i<201;i++){
+   if(counting[i]==it.first){
+    int val=it.first;
+        while(val--){
+        ans.push_back(i-100);
+        }
+    }
+}
+            }
+
+        }
+        return ans;
+
+
+    }
+};
