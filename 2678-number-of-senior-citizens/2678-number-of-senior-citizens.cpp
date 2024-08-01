7c7e24034e6adccad0361a489751c65604b25711
Jainam <85880788+JAINAM576@users.noreply.github.com>
2024-08-01 19:38:22 +0530

    Time: 4 ms (82.75%), Space: 17.5 MB (31.37%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count=0;
+        for(int i=0;i<details.size();i++){
+            if(details[i][11]>='6' && details[i][12]>='0' ){
+                count++;
+            }
+               if(details[i][11]=='6' && details[i][12]=='0' ){
+                count--;
+            }
+        }
+        return count;
+    }
+
+};
