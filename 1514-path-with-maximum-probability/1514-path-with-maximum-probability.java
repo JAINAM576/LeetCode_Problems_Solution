95732be3059c92a6611998f63f3c2d6b0d10bf5b
Jainam <85880788+JAINAM576@users.noreply.github.com>
2024-08-27 22:51:56 +0530

    Time: 10 ms (98.38%), Space: 55.8 MB (97.39%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -2,9 +2,9 @@ class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
         double[] bellman = new double[n];
         bellman[start_node] = 1.0;
-        
         for (int i = 0; i < n - 1; i++) {
             int j = 0;
+        boolean isChange=false;
             for (int[] edge : edges) {
                 int u = edge[0];
                 int v = edge[1];
@@ -12,13 +12,16 @@ class Solution {
                 
                 if (bellman[u] * sp > bellman[v]) {
                     bellman[v] = bellman[u] * sp;
+                    isChange=true;
                 }
                 
                 if (bellman[v] * sp > bellman[u]) {
                     bellman[u] = bellman[v] * sp;
+                    isChange=true;
                 }
                 j++;
             }
+            if(!isChange)break;
         }
         
         return bellman[end_node];
