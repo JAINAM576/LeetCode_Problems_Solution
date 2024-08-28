41359888c86810cd95efd280584f3d1552197228
Jainam <85880788+JAINAM576@users.noreply.github.com>
2024-08-28 11:06:55 +0530

    Time: 208 ms (75.98%), Space: 93.9 MB (81.49%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+void dfs(int i,int j,int n,int m, vector<vector<int>>& grid2){
+if(i>=n || i<0 || j<0 || j>=m)return ;
+
+grid2[i][j]=0;
+if((i-1 )>=0 &&  grid2[i-1][j]==1){
+dfs(i-1,j,n,m,grid2);
+}
+if((j-1 )>=0 &&  grid2[i][j-1]==1){
+dfs(i,j-1,n,m,grid2);
+}
+if((i+1 )<n &&  grid2[i+1][j]==1){
+dfs(i+1,j,n,m,grid2);
+}
+if((j+1 )<m &&  grid2[i][j+1]==1){
+dfs(i,j+1,n,m,grid2);
+}
+}
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+     int count=0;
+     int n=grid1.size();
+     int m=grid1[0].size();
+     for(int i=0;i<n;i++){
+        // vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int j=0;j<m;j++){
+            if(grid1[i][j]==0 && grid2[i][j]==1){
+                dfs(i,j,n,m,grid2);
+            }
+        }
+     }
+
+    for(int i=0;i<n;i++){
+        // vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int j=0;j<m;j++){
+            if(grid2[i][j]==1){
+                dfs(i,j,n,m,grid2);
+                count++;
+            }
+        }
+     }
+     return count;
+
+
+
+    }
+};
